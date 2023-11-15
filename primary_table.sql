b48f0dbd57b40f6fc4be3374dd2bab40edab8ad9
tinkaxx <146669179+tinkaxx@users.noreply.github.com>
2023-11-15 10:28:38 +0100

    create tables for price and wage

--- primary_table.sql
+++ primary_table.sql
@@ -1,17 +1,16 @@
-CREATE TABLE t_Kristyna_Stefkova_project_sql_primary_final AS
-
+CREATE TABLE t_Kristyna_Stefkova_project_sql_average_price AS
 SELECT
 	cp.category_code,
 	cpc.name,
 	YEAR (date_from) AS YEAR,
-	round(avg(value),1) AS average_value
+	round(avg(value),1) AS average_price
 FROM czechia_price cp
 JOIN czechia_price_category cpc 
 	ON cp.category_code = cpc.code
-JOIN czechia_payroll cp2 
 GROUP BY category_code, YEAR
 ORDER BY YEAR DESC;
 
+CREATE TABLE t_Kristyna_Stefkova_project_sql_average_wage AS
 SELECT 
 	cp.industry_branch_code,
 	cpib.name,
@@ -22,3 +21,4 @@ JOIN czechia_payroll_industry_branch cpib
 	ON cpib.code = cp.industry_branch_code 
 WHERE value_type_code = 5958 AND unit_code = 200 AND calculation_code = 100
 GROUP BY industry_branch_code, payroll_year ;
+
