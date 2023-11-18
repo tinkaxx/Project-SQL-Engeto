8d8bfd67139a2789b4280184bae7f74c2cca1497
tinkaxx <146669179+tinkaxx@users.noreply.github.com>
2023-11-18 22:00:15 +0100

    create table for task 4

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT 
+	tkspt.payroll_year,
+	round(avg(tkspt.percentage_salary_increase),2) AS AVG_percentage_salary_increase,
+	tkspt2.`year`,
+	round(avg(tkspt2.percentage_interannual_growth),2) AS AVG_percentage_price_growth,
+	round((avg(tkspt2.percentage_interannual_growth) - avg(tkspt.percentage_salary_increase)),2) AS price_salary_difference
+FROM t_kristyna_stefkova_project_task_1 tkspt
+JOIN t_kristyna_stefkova_project_task_3 tkspt2 
+	ON tkspt.payroll_year = tkspt2.`year`
+	AND tkspt.last_year = tkspt2.last_year
+GROUP BY payroll_year 
+;
+
