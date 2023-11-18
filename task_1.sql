65be661778b178cf0182ef65f12dd0a9b7e6d949
tinkaxx <146669179+tinkaxx@users.noreply.github.com>
2023-11-18 20:28:44 +0100

    task 1 was modified

--- task_1.sql
+++ task_1.sql
@@ -6,11 +6,11 @@ SELECT DISTINCT
 	tkspspf2.average_wage,
 	tkspspf.payroll_year AS last_year, 
 	tkspspf.average_wage AS previous_AVG_wage,
-	tkspspf2.average_wage/tkspspf.average_wage AS salary_increase
+	round(((tkspspf2.average_wage-tkspspf.average_wage)/tkspspf.average_wage*100),2) AS percentage_salary_increase
 FROM t_kristyna_stefkova_project_sql_primary_final tkspspf
 JOIN t_kristyna_stefkova_project_sql_primary_final tkspspf2 
 	ON tkspspf.`payroll_year` = tkspspf2.`payroll_year` - 1
 	AND tkspspf.industry_name = tkspspf2.industry_name
-WHERE (tkspspf2.average_wage/tkspspf.average_wage) < 1
-GROUP BY tkspspf.industry_name, tkspspf.`year`;
+GROUP BY tkspspf.industry_name, tkspspf.`year`
+ORDER BY percentage_salary_increase;
 
