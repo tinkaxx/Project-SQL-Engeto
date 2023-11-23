38091989838af5e12a902dea13e5c6e14f11bffe
tinkaxx <146669179+tinkaxx@users.noreply.github.com>
2023-11-23 23:11:19 +0100

    create task 5

--- task_5.sql
+++ task_5.sql
@@ -34,13 +34,13 @@ SELECT
 	tkspssf2.`year` AS `year`,
 	tkspssf2.GDP_mil_dollars,
     round(((tkspssf2.GDP_mil_dollars-tkspssf.GDP_mil_dollars)/tkspssf.GDP_mil_dollars*100),2) AS GDP__percentage_difference,
-    round(avg(average_price),2)
+    round(avg(average_price),2) AS average_price_1 
 FROM t_kristyna_stefkova_project_sql_secondary_final tkspssf 
 JOIN t_kristyna_stefkova_project_sql_secondary_final tkspssf2 
 	ON tkspssf .`year` = tkspssf2 .`year` - 1 
 	AND tkspssf.country = tkspssf2.country
 JOIN t_kristyna_stefkova_project_sql_average_price tkspsap 
-	ON tkspsap.`YEAR` - 1 = tkspssf.`year` 
+	ON tkspsap.`YEAR` = tkspssf2.`year` 
 WHERE tkspssf.country = "Czech Republic"
 GROUP BY `YEAR` ;
 
@@ -49,3 +49,26 @@ SELECT
 	 round(avg(average_price),2)
 FROM t_kristyna_stefkova_project_sql_average_price tkspsap 
 GROUP BY `YEAR` 
+
+
+SELECT 
+	tkspssf2.`year` AS `year`,
+	tkspssf2.GDP_mil_dollars,
+	tkspssf.GDP_mil_dollars AS previous_GDP_mil_dollars,
+    round(((tkspssf2.GDP_mil_dollars-tkspssf.GDP_mil_dollars)/tkspssf.GDP_mil_dollars*100),2) AS percentage_GDP_difference,
+    round(avg(average_price),2) AS average_price,
+    round(AVG(previous_AVG_price),2) AS previous_AVG_price,
+    round(((round(avg(average_price),2) - round(AVG(previous_AVG_price),2))/round(AVG(previous_AVG_price),2)*100),2) AS percentage_price_difference,
+    round(avg(average_wage),2) AS average_wage,
+    round(avg(previous_AVG_wage),2) AS previous_AVG_wage,
+    round(((round(avg(average_wage) - round(avg(previous_AVG_wage),2)))/round(avg(previous_AVG_wage),2)*100),2) AS percentage_wage_difference
+FROM t_kristyna_stefkova_project_sql_secondary_final tkspssf 
+JOIN t_kristyna_stefkova_project_sql_secondary_final tkspssf2 
+	ON tkspssf .`year` = tkspssf2 .`year` - 1 
+	AND tkspssf.country = tkspssf2.country
+JOIN t_kristyna_stefkova_project_task_3 tkspt 
+	ON tkspt.`year` = tkspssf2.`year` 
+JOIN t_kristyna_stefkova_project_task_1 tkspt2 
+	ON tkspt2.payroll_year = tkspssf2.`year` 
+WHERE tkspssf.country = "Czech Republic"
+GROUP BY `YEAR` ;
